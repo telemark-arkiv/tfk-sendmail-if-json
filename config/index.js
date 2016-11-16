46af9f05e1fa129096f4fdbb6a27242b6ac8dc1f
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-16 15:17:53 +0100

    Changes defaults

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   DIRECTORY_PATH: process.env.DIRECTORY_PATH || 'test/data/files',
-  MAIL_TO: process.env.MAIL_TO || 'jonas.enge@t-fk.no',
-  MAIL_FROM: process.env.MAIL_FROM || 'minelev@t-fk.no',
+  MAIL_TO: process.env.MAIL_TO || 'jonas.enge@gmail.com',
+  MAIL_FROM: process.env.MAIL_FROM || 'botolf@t-fk.no',
   API_KEY: process.env.API_KEY || 'your-sendgrid-api-key'
 }
