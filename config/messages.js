79cd64f3fe367c41b2c7613c0012663d4e1ada7a
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-16 15:45:34 +0100

    Adds more messages

--- config/messages.js
+++ config/messages.js
@@ -3,6 +3,11 @@
 module.exports = (data) => [
   `Kunne du hjulpet meg litt? Det har seg slik at jeg har funnet ${data.numberOfFiles} filer i mappen ${data.directoryPath}`,
   `Har du noe tid til overs? Jeg har oppdaget ${data.numberOfFiles} filer i mappen ${data.directoryPath}`,
+  `Pssssst.... kompis? Er du hypp på ${data.numberOfFiles} filer i mappa ${data.directoryPath}?`,
+  `Yo, yo, yo! Det er jaggu ${data.numberOfFiles} filer i mappa ${data.directoryPath}`,
+  `... og sannelig jeg sier eder: ${data.numberOfFiles} er tallet står det skrevet i ${data.directoryPath}`,
+  `U fix plz? ${data.numberOfFiles} filer i mappen ${data.directoryPath}`,
+  `Jeg trenger kos og kjærlighet. Det ligger ${data.numberOfFiles} filer i mappen ${data.directoryPath}`,
   `Vil du leke en lek? Det er ${data.numberOfFiles} filer i mappen ${data.directoryPath} kan du gjette hvorfor?`,
   `Er det mandag? Kan virke slik. Det er nemlig ${data.numberOfFiles} filer i mappen ${data.directoryPath}`,
   `Unnskyld meg? Kan du kikke på de ${data.numberOfFiles} filene i mappen ${data.directoryPath}?`
